2f0027a3ff401ffe94f8f2885a5d6e728b1add6f
luizlaljr <luizlaljr@gmail.com>
2020-11-15 10:09:49 -0300

    Definida a rota inicial

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ server.use(cors());
 const url = 'http://queimadas.dgi.inpe.br/queimadas/dados-abertos/download/?utm_campaign=dados-abertos&outputFormat=json&utm_medium=landing-page&time=48h&utm_content=focos_brasil_48h&id=focos_brasil&utm_source=landing-page'; 
 
 
-server.get('/index',async(_req,res) => {
+server.get('/',async(_req,res) => {
     try {
         const {data} = await axios(url);
         console.log(data);
