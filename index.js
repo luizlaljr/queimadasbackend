a0af3fc322c3f51f9c64d0cb6ea157f1f4f87468
luizlaljr <luizlaljr@gmail.com>
2020-11-15 09:56:45 -0300

    Criacao do servidor de foco de queimadas

---
+++ index.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const express = require('express');
+const cors = require('cors');
+const server = express();
+
+server.use(cors());
+
+const url = 'http://queimadas.dgi.inpe.br/queimadas/dados-abertos/download/?utm_campaign=dados-abertos&outputFormat=json&utm_medium=landing-page&time=48h&utm_content=focos_brasil_48h&id=focos_brasil&utm_source=landing-page'; 
+
+
+server.get('/index',async(_req,res) => {
+    try {
+        const {data} = await axios(url);
+        console.log(data);
+        res.json(data);
+    } catch (error) {
+       console.log(error);
+    }
+    
+});
+
+server.listen(process.env.PORT || 3333);
+
