b262a81896537be38effa0eaa49f5772b486d4b2
luizlaljr <luizlaljr@gmail.com>
2020-11-21 18:35:20 -0300

    Fixado bug no servidor

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const server = express();
 
-require('../app');
-
 server.use(cors());
 
 const url = 'http://queimadas.dgi.inpe.br/queimadas/dados-abertos/download/?utm_campaign=dados-abertos&outputFormat=json&utm_medium=landing-page&time=48h&utm_content=focos_brasil_48h&id=focos_brasil&utm_source=landing-page'; 
